8f17e4b79fb391672691de700506670660548577
Tomáš Honsa <Tomas.Honsa@student.souepl.cz>
2024-11-26 13:43:43 +0100

    m

--- main.py
+++ main.py
@@ -3,15 +3,38 @@ import machine
 from time import sleep
 sensor = dht.DHT11(machine.Pin(4))
 
+led1 = machine.Pin(0, machine.Pin.OUT)
+led2 = machine.Pin(1, machine.Pin.OUT)
+led3 = machine.Pin(2, machine.Pin.OUT)
+led4 = machine.Pin(3, machine.Pin.OUT)
+
+
 while True:
-  try:
-    sleep(3)
-    sensor.measure()
-    temp = sensor.temperature()
-    hum = sensor.humidity()
-    temp_f = temp * (9/5) + 32.0
-    print('Temperature: %3.1f C' %temp)
-    print('Temperature: %3.1f F' %temp_f)
-    print('Humidity: %3.1f %%' %hum)
-  except OSError as e:
-    print('Failed to read sensor.')
+  sensor.measure()
+  temp = sensor.temperature()
+  hum = sensor.humidity()
+  print('teplota: %3.1f' %temp)
+  print('vlhkost: %3.1f %%' %hum)
+  teplota=('%3.1f'%temp)
+  vlhkost=('%3.1f %%' %hum)
+  print(teplota)
+  if teplota=="22.0":
+    led1.value(1)
+    sleep(2)
+    led1.value(0)
+  elif teplota=="23.0":
+    led1.value(1)
+    led2.value(1)
+    sleep(2)
+    led1.value(0)
+    led2.value(0)
+  elif teplota=="24.0":
+    led1.value(1)
+    led2.value(1)
+    led3.value(1)
+    sleep(2)
+    led1.value(0)
+    led2.value(0)
+    led3.value(0)
+  else:
+    print("nic")
