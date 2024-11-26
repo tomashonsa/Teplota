1c5912e48fb28964c3602a41454696cabdc5bbb0
Tomáš Honsa <Tomas.Honsa@student.souepl.cz>
2024-11-26 12:38:34 +0100

    s

---
+++ main.py
@@ -0,0 +1,17 @@
+import dht
+import machine
+from time import sleep
+sensor = dht.DHT11(machine.Pin(4))
+
+while True:
+  try:
+    sleep(3)
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    temp_f = temp * (9/5) + 32.0
+    print('Temperature: %3.1f C' %temp)
+    print('Temperature: %3.1f F' %temp_f)
+    print('Humidity: %3.1f %%' %hum)
+  except OSError as e:
+    print('Failed to read sensor.')
